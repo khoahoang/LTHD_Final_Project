d5ca381eab812c7f54aa2a794bf925f84e3adaec
tuankiet234 <tuankiet234@gmail.com>
2016-01-02 16:49:46 +0700

    sync

---
+++ Server/Authentication/Authentication/Models/ProductDetailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Authentication.Models
+{
+    public class ProductDetailModel
+    {
+        public string NSX { get; set; }
+        public string MaSP { get; set; }
+        public string TinhTrang { get; set; }
+
+    }
+}
