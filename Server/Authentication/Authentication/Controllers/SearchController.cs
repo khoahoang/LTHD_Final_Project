dc16fe51a9fbd1b2e0000f846ab425fea25799c6
origin <khoahoang06@gmail.com>
2016-01-02 09:43:42 +0700

    search + paging

---
+++ Server/Authentication/Authentication/Controllers/SearchController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Authentication.Models;
+using PagedList;
+
+namespace Authentication.Controllers
+{
+    public class SearchController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Search(string keyword, int? page)
+        {
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            MobileStoreServiceEntities sv = new MobileStoreServiceEntities();
+            List<PRODUCT> lstProduct = sv.PRODUCTs.Where(x => x.MODEL.Contains(keyword)).ToList();
+
+            return CreateResponse(HttpStatusCode.OK, lstProduct.ToPagedList(pageNumber, pageSize));
+        }
+        [HttpGet]
+        public HttpResponseMessage SearchPage(string keyword)
+        {
+            MobileStoreServiceEntities sv = new MobileStoreServiceEntities();
+            List<PRODUCT> lstProduct = sv.PRODUCTs.Where(x => x.MODEL.Contains(keyword)).ToList();
+
+            return CreateResponse(HttpStatusCode.OK, lstProduct);
+        }
+        #region Helper
+        public HttpResponseMessage CreateResponse<T>(HttpStatusCode statusCode, T data)
+        {
+            return Request.CreateResponse(statusCode, data);
+        }
+
+        public HttpResponseMessage CreateResponse(HttpStatusCode httpStatusCode)
+        {
+            return Request.CreateResponse(httpStatusCode);
+        }
+        #endregion
+    }
+}
