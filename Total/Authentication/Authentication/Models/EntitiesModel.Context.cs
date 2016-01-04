fbbaf14a4d7ee2bfbeeeddb2a9f98a2048855af9
origin <khoahoang06@gmail.com>
2016-01-04 22:18:12 +0700

    fix git

--- Total/Authentication/Authentication/Models/EntitiesModel.Context.cs
+++ Total/Authentication/Authentication/Models/EntitiesModel.Context.cs
@@ -25,11 +25,6 @@ namespace Authentication.Models
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
-        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
-        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
-        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
-        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
         public virtual DbSet<ATTRIBUTE> ATTRIBUTEs { get; set; }
         public virtual DbSet<CART> CARTs { get; set; }
         public virtual DbSet<CATEGORY> CATEGORies { get; set; }
--- Total/Authentication/Authentication/Models/EntitiesModel.Designer.cs
+++ Total/Authentication/Authentication/Models/EntitiesModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Hoc\Subject\Lap_trinh_hien_dai\Project\Project_folder\LTHD_Final_Project\Total\Authentication\Authentication\Models\EntitiesModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Hoc\Subject\Lap_trinh_hien_dai\Project\Project_folder\Folder_Project\LTHD_Final_Project\Total\Authentication\Authentication\Models\EntitiesModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
