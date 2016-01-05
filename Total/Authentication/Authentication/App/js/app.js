570dcd2c6925660d5e11da39e7e3bc68ebc2ffb6
origin <khoahoang06@gmail.com>
2016-01-05 17:30:10 +0700

    web extraction from genk

--- Total/Authentication/Authentication/App/js/app.js
+++ Total/Authentication/Authentication/App/js/app.js
@@ -80,7 +80,22 @@ mobileStoreApp.run(['authService', function (authService) {
     authService.fillAuthData();
     authService.setDataShopping();
 }]);
+/*
+ .run( function($rootScope, $location) {
 
+    // register listener to watch route changes
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      if ( $rootScope.loggedUser == null ) {
+        // no logged user, we should be going to #login
+        if ( next.templateUrl == "partials/login.html" ) {
+          // already going to #login, no redirect needed
+        } else {
+          // not going to #login, we should redirect now
+          $location.path( "/login" );
+        }
+      }         
+    });
+    */
 mobileStoreApp.config(function ($httpProvider) {
     $httpProvider.interceptors.push('authInterceptorService');
 });
